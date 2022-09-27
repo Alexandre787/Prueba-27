b4d8bd1a887374a96e54ffeae8fd4ab2a148871d
Alexandre787 <113344727+Alexandre787@users.noreply.github.com>
2022-09-27 10:02:59 +0200

    Lados y descuentos ejemplos
    Lados y descuentos

---
+++ ConsoleApp2/ConsoleApp2/Module1.vb
@@ -0,0 +1,24 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim catetoA, catetoB, hipotenusa As Double
+        Console.WriteLine("Introduzca valor cateto A: ")
+        catetoA = Convert.ToDouble(Console.ReadLine())
+        Console.WriteLine("Introduzca valor cateto B: ")
+        catetoB = Convert.ToDouble(Console.ReadLine)
+
+        'Calcula
+        'hipotenusa = Math.Pow(catetoA, 2) + Math.Pow(catetoB, 2)
+        'hipotenusa = Math.Sqrt(hipotenusa)
+        hipotenusa = Math.Sqrt(Math.Pow(catetoA, 2) + Math.Pow(catetoB, 2))
+        hipotenusa = Math.Round(hipotenusa, 2)
+        Console.WriteLine("La hipotenusa es: " & hipotenusa)
+        Console.ReadLine()
+
+
+
+
+
+    End Sub
+
+End Module
---
+++ ConsoleApp2/ConsoleApp2/Module2.vb
@@ -0,0 +1,19 @@
+﻿Module Module2
+
+    Sub Main()
+        Dim precioInicial, precioFinal As Single
+        Dim descuento As Byte
+
+        'Entrada:
+        Console.WriteLine("Intro precio inicial: ")
+        precioInicial = Convert.ToSingle(Console.ReadLine)
+        Console.WriteLine("Introduzca descuento: ")
+        descuento = Convert.ToByte(Console.ReadLine)
+
+        precioFinal = precioInicial - descuento - (descuento * precioInicial) / 100
+
+        Console.WriteLine("El precio final es de : " & precioFinal & "euros")
+        Console.ReadLine()
+
+    End Sub
+End Module
